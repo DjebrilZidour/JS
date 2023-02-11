66c8078e0183479d0e886349c32cffbcd998b268
Djebril <djebrilacademy@gmail.com>
2023-02-11 11:04:41 +0100

    intro to js

---
+++ script.js
@@ -0,0 +1,20 @@
+var A = 12;
+var B = 7;
+var C = 12 % 7;
+var DeviceName = "Samsung s 10";
+var version = "2";
+
+console.log(DeviceName);
+
+function sum() {
+    var C = A + B;
+    console.log(C)
+}
+
+function ExecuteLoop(){
+    for (var i = 0; i <= 5; i = i + 2 ) { // 0 2 4
+        console.log(i)
+    }
+}
+sum();
+ExecuteLoop();
